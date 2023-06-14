330226b3d76824def8991303b8cd0ae8e8059f28
FanFauzi <if3309291@gmail.com>
2023-06-14 17:36:02 +0700

    data validation implemented

--- src/validator/notes/index.js
+++ src/validator/notes/index.js
@@ -1,5 +1,13 @@
-const NotesValidator = {
-    validateNotePayload: (payload) => {
+const InvariantError = require('../../exceptions/InvariantError');
+const { NotePayloadShema } = require('./schema');
 
-    },
+const NotesValidator = {
+  validateNotePayload: (payload) => {
+    const validationResult = NotePayloadShema.validate(payload);
+    if (validationResult.error) {
+      throw new InvariantError(validationResult.error.message);
+    }
+  },
 };
+
+module.exports = NotesValidator;
